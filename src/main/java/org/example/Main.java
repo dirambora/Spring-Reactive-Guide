3c786ee9812c8cf9790e872396418ad50bbbe723
dirambora <dirambora3@gmail.com>
2024-03-07 16:59:37 +0300

    flux and mono

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -11,7 +14,13 @@ public class Main {
         Flux<Integer> just = Flux.just(1, 2, 3, 4);
 
         //Mono- a stream that can emit 0..1 elements
-//        Mono<Integer> just =Mono.just();
+//        Mono<Integer> just =Mono.just(1);
 
+        //collecting elements
+        //use the subscribe method for elements to start flowing
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(elements::add);
     }
 }
